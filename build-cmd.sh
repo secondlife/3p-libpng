5822c9ce636e2f32122f6be5a9127725228c22fd
Rye Mutt <rye@alchemyviewer.org>
2024-08-09 03:57:25 -0400

    Rework build script to use cmake for all platforms

--- build-cmd.sh
+++ build-cmd.sh
@@ -10,7 +10,7 @@ if [ -z "$AUTOBUILD" ] ; then
     exit 1
 fi
 
-if [ "$OSTYPE" = "cygwin" ] ; then
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]] ; then
     autobuild="$(cygpath -u $AUTOBUILD)"
 else
     autobuild="$AUTOBUILD"
@@ -28,139 +28,97 @@ source "$(dirname "$AUTOBUILD_VARIABLES_FILE")/functions"
 [ -f "$stage"/packages/include/zlib-ng/zlib.h ] || \
 { echo "Run 'autobuild install' first." 1>&2 ; exit 1; }
 
-# One of makefiles looks for zlib in zlib\zlib.lib
-if [ -d "$stage"/packages/include/zlib ] ; then
-    rm -r "$stage"/packages/include/zlib
-fi
-cp -r "$stage"/packages/include/zlib-ng "$stage"/packages/include/zlib
-
-# Restore all .sos
-restore_sos ()
-{
-    for solib in "${stage}"/packages/lib/{debug,release}/lib*.so*.disable; do 
-        if [ -f "$solib" ]; then
-            mv -f "$solib" "${solib%.disable}"
-        fi
-    done
-}
-
-
-# Restore all .dylibs
-restore_dylibs ()
-{
-    for dylib in "$stage/packages/lib"/{debug,release}/*.dylib.disable; do
-        if [ -f "$dylib" ]; then
-            mv "$dylib" "${dylib%.disable}"
-        fi
-    done
-}
-
 pushd "$PNG_SOURCE_DIR"
     case "$AUTOBUILD_PLATFORM" in
 
         windows*)
             load_vsvars
 
-            TARGET_CPU=X64 \
-            INCLUDE="${INCLUDE:-};$(cygpath -w $stage/packages/include/zlib)" \
-            RUNTIME_LIBS=static \
-            LINK="${LINK:-};$(cygpath -w $stage/packages/lib/release)" \
-            nmake -f scripts/makefile.vcwin32
+            opts="$(replace_switch /Zi /Z7 $LL_BUILD_RELEASE)"
+            plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
 
-            mkdir -p "$stage/lib/release"
+            mkdir -p "build"
+            pushd "build"
+                cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release \
+                    -DCMAKE_C_FLAGS:STRING="$plainopts" \
+                    -DCMAKE_CXX_FLAGS:STRING="$opts" \
+                    -DPNG_SHARED=ON \
+                    -DPNG_HARDWARE_OPTIMIZATIONS=ON \
+                    -DZLIB_INCLUDE_DIR="$(cygpath -m "$stage/packages/include/zlib-ng/")" \
+                    -DZLIB_LIBRARY="$(cygpath -m "$stage/packages/lib/release/zlib.lib")" \
+                    -DCMAKE_INSTALL_PREFIX=$(cygpath -m $stage)
+            
+                cmake --build . --config Release
 
-            cp libpng.lib "$stage/lib/release/libpng16.lib"
-            mkdir -p "$stage/include/libpng16"
-            cp -a {png.h,pngconf.h,pnglibconf.h} "$stage/include/libpng16"
+                # conditionally run unit tests
+                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                    ctest -C Release
+                fi
+
+                cmake --install . --config Release
+
+                mkdir -p $stage/lib/release/
+                mv $stage/lib/libpng16_static.lib "$stage/lib/release/libpng16.lib"
+            popd
         ;;
 
         darwin*)
+            mkdir -p "build"
+            pushd "build"
+
+            export MACOSX_DEPLOYMENT_TARGET="$LL_BUILD_DARWIN_DEPLOY_TARGET"
+
             opts="${TARGET_OPTS:--arch $AUTOBUILD_CONFIGURE_ARCH $LL_BUILD_RELEASE}"
-            export CC=clang++
 
-            # Force libz static linkage by moving .dylibs out of the way
-            # (Libz is currently packaging only statics but keep this alive...)
-            trap restore_dylibs EXIT
-            for dylib in "$stage"/packages/lib/{debug,release}/libz*.dylib; do
-                if [ -f "$dylib" ]; then
-                    mv "$dylib" "$dylib".disable
-                fi
-            done
+            cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release \
+                -DCMAKE_C_FLAGS:STRING="$(remove_cxxstd $opts)" \
+                -DCMAKE_CXX_FLAGS:STRING="$opts" \
+                -DPNG_SHARED=ON \
+                -DPNG_HARDWARE_OPTIMIZATIONS=ON \
+                -DZLIB_INCLUDE_DIR="$stage/packages/include/zlib-ng/" \
+                -DZLIB_LIBRARY="$stage/packages/lib/release/libz.a" \
+                -DCMAKE_INSTALL_PREFIX="$stage" \
+                -DCMAKE_INSTALL_LIBDIR="$stage/lib/release" \
+                -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+                -DCMAKE_OSX_ARCHITECTURES="x86_64"
 
-            # See "linux" section for goals/challenges here...
-            CFLAGS="$(remove_cxxstd $opts)" \
-                CXXFLAGS="$opts" \
-                CPPFLAGS="${CPPFLAGS:-} -I$stage/packages/include/zlib" \
-                LDFLAGS="-L$stage/packages/lib/release" \
-                ./configure --prefix="$stage" --libdir="$stage/lib/release" \
-                            --with-zlib-prefix="$stage/packages" --enable-shared=no --with-pic
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
+            cmake --build . --config Release
 
-            # clean the build artifacts
-            make distclean
+            # conditionally run unit tests
+            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                ctest -C Release
+            fi
+
+            cmake --install . --config Release
+            popd
         ;;
 
         linux*)
-            # Linux build environment at Linden comes pre-polluted with stuff that can
-            # seriously damage 3rd-party builds.  Environmental garbage you can expect
-            # includes:
-            #
-            #    DISTCC_POTENTIAL_HOSTS     arch           root        CXXFLAGS
-            #    DISTCC_LOCATION            top            branch      CC
-            #    DISTCC_HOSTS               build_name     suffix      CXX
-            #    LSDISTCC_ARGS              repo           prefix      CFLAGS
-            #    cxx_version                AUTOBUILD      SIGN        CPPFLAGS
-            #
-            # So, clear out bits that shouldn't affect our configure-directed build
-            # but which do nonetheless.
-            #
-            # unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
+            mkdir -p "build"
+            pushd "build"
 
             # Default target per AUTOBUILD_ADDRSIZE
             opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
 
-            # Handle any deliberate platform targeting
-            if [ -z "${TARGET_CPPFLAGS:-}" ]; then
-                # Remove sysroot contamination from build environment
-                unset CPPFLAGS
-            else
-                # Incorporate special pre-processing flags
-                export CPPFLAGS="$TARGET_CPPFLAGS"
+            cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release \
+                -DCMAKE_C_FLAGS:STRING="$(remove_cxxstd $opts)" \
+                -DCMAKE_CXX_FLAGS:STRING="$opts" \
+                -DPNG_SHARED=ON \
+                -DPNG_HARDWARE_OPTIMIZATIONS=ON \
+                -DZLIB_INCLUDE_DIR="$stage/packages/include/zlib-ng/" \
+                -DZLIB_LIBRARY="$stage/packages/lib/release/libz.a" \
+                -DCMAKE_INSTALL_PREFIX="$stage" \
+                -DCMAKE_INSTALL_LIBDIR="$stage/lib/release"
+
+            cmake --build . --config Release
+
+            # conditionally run unit tests
+            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                ctest -C Release
             fi
 
-            # Force static linkage to libz by moving .sos out of the way
-            # (Libz is only packaging statics right now but keep this working.)
-            trap restore_sos EXIT
-            for solib in "${stage}"/packages/lib/{debug,release}/libz.so*; do
-                if [ -f "$solib" ]; then
-                    mv -f "$solib" "$solib".disable
-                fi
-            done
-
-            # 1.16 INSTALL claims ZLIBINC and ZLIBLIB env vars are active but this is not so.
-            # If you fail to pick up the correct version of zlib (from packages), the build
-            # will find the system's version and generate the wrong PNG_ZLIB_VERNUM definition
-            # in the build.  Mostly you won't notice until certain things try to run.  So
-            # check the generated pnglibconf.h when doing development and confirm it's correct.
-            #
-            # The sequence below has the effect of:
-            # * Producing only static libraries.
-            # * Builds all bin/* targets with static libraries.
-
-            # build the release version and link against the release zlib
-            CFLAGS="$(remove_cxxstd $opts)" \
-                CXXFLAGS="$opts" \
-                CPPFLAGS="${CPPFLAGS:-} -I$stage/packages/include/zlib" \
-                LDFLAGS="-L$stage/packages/lib/release" \
-                ./configure --prefix="$stage" --libdir="$stage/lib/release" \
-                            --includedir="$stage/include" --enable-shared=no --with-pic
-
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
-
-            # clean the build artifacts
-            make distclean
+            cmake --install . --config Release
+            popd
         ;;
     esac
     mkdir -p "$stage/LICENSES"
